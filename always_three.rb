f7eb3739273568c86b210fdbef0b61a043728023
Myshawne <Myshawnestallings@gmail.com>
2019-10-16 13:53:22 -0700

    add parameters to method

--- always_three.rb
+++ always_three.rb
@@ -1,11 +1,13 @@
-def always_3
 
     puts "Enter a number"
 
     num1 = gets.to_i
 
+def always_3(num1)
+
+    
     puts "Always  #{((((num1 + 5) * 2) - 4) / 2 - num1).to_s}"
 
 end
 
-always_3
+always_3(num1)
