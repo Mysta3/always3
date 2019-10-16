cc35b2ba0d6801a5deaf105db03a4f23c7bd92a5
Myshawne <Myshawnestallings@gmail.com>
2019-10-16 13:29:22 -0700

    refactored w/ methods

--- always_three.rb
+++ always_three.rb
@@ -1,7 +1,11 @@
-puts "Enter a number"
+def always_3
 
-num1 = gets.to_i
+    puts "Enter a number"
 
-puts "Always  #{((((num1 + 5) * 2) - 4) / 2 - num1).to_s}"
+    num1 = gets.to_i
 
+    puts "Always  #{((((num1 + 5) * 2) - 4) / 2 - num1).to_s}"
 
+end
+
+always_3
